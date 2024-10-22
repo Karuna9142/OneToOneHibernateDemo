5f7af97586f9b706a4a7bb1488772a06ee7eab70
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-22 22:29:40 +0530

    Add files via upload

---
+++ OneDemo.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class OneDemo {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg =  new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session s = sf.openSession();
+		
+		Transaction t = s.beginTransaction();
+		
+		Student s1 = new Student();
+		Subject s2 = new Subject();
+		
+		
+		s2.setSubid(1); s2.setSname("English"); s2.setSmarks(80);
+		s.save(s2);
+		
+		s1.setStudid(101); s1.setStudname("Krishna"); s1.setStuage(22); s1.setSub(s2);
+		s.save(s1);
+		
+		t.commit();
+		s.close();
+
+	}
+
+}
